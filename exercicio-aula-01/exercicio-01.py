30b1922c4d92e338fecee975e85eb37ec0d10b7f
Ivam Ferreira de Souza <ivamsouza.xyz@gmail.com>
2021-08-15 20:30:58 -0400

    Exercício 01 de Python em aula

---
+++ exercicio-aula-01/exercicio-01.py
@@ -0,0 +1,7 @@
+nome = input('\nQual é o seu nome? ')
+print('Olá!',nome,'!' 'Prazer em conhecê-lo!\n')
+
+dia = input('Que dia você nasceu? ')
+mes = input('De qual mês? ')
+ano = input('De qual ano? ')
+print('\nVocê nasceu no dia',dia,'de',mes,'de',ano,'.','Está correto?\n')
